dfba81d47b266e257eb50d24fae1b918b7dec6c4
Maurilio <maurilioz1@hotmail.com>
2023-11-21 15:23:32 -0300

    create notification service

---
+++ src/ManageBidding.Core/DomainObjects/Interfaces/Services/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using ManageBidding.Core.DomainObjects.Models;
+
+namespace ManageBidding.Core.DomainObjects.Interfaces.Services
+{
+    public interface INotificationService
+    {
+        bool HaveNotification();
+        List<Notification> GetNotification();
+        void Handle(Notification notification);
+    }
+}
---
+++ src/ManageBidding.Core/DomainObjects/Services/NotificationService.cs
@@ -0,0 +1,30 @@
+﻿using ManageBidding.Core.DomainObjects.Models;
+using ManageBidding.Core.DomainObjects.Interfaces.Services;
+
+namespace ManageBidding.Core.DomainObjects.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private List<Notification> _notifications;
+
+        public NotificationService()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public bool HaveNotification()
+        {
+            return _notifications.Any();
+        }
+
+        public List<Notification> GetNotification()
+        {
+            return _notifications;
+        }
+
+        public void Handle(Notification notification)
+        {
+            _notifications.Add(notification);
+        }
+    }
+}
