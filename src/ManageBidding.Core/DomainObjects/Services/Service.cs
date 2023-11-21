26b0bea6705b19bcfac927ccdcb24fc03edb8b0a
Maurilio <maurilioz1@hotmail.com>
2023-11-21 15:32:30 -0300

    create base service

---
+++ src/ManageBidding.Core/DomainObjects/Services/Service.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ManageBidding.Core.DomainObjects.Models;
+using ManageBidding.Core.DomainObjects.Interfaces.Services;
+
+namespace ManageBidding.Core.DomainObjects.Services
+{
+    public abstract class Service
+    {
+        private readonly INotificationService _notification;
+
+        public Service(INotificationService notification)
+        {
+            _notification = notification;
+        }
+
+        protected void Notify(string message)
+        {
+            _notification.Handle(new Notification(message));
+        }
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notify(error.ErrorMessage);
+            }
+        }
+
+        protected bool ExecuteValidation<TV, TE>(TV validation, TE entity) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validation.Validate(entity);
+
+            if(validator.IsValid)
+            {
+                return true;
+            }
+
+            Notify(validator);
+
+            return false;
+        }
+    }
+}
