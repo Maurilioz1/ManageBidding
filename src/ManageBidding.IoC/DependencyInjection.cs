81bf102222b676d80488a1c1b4229e16e4ce6085
Maurilio <maurilioz1@hotmail.com>
2023-11-25 11:38:57 -0300

    create dependency injection

---
+++ src/ManageBidding.IoC/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ManageBidding.Domain.Interfaces;
+using ManageBidding.Core.DomainObjects.Services;
+using ManageBidding.Data.EntityFramework.Context;
+using ManageBidding.Data.EntityFramework.Repositories;
+using ManageBidding.Core.DomainObjects.Interfaces.Services;
+
+namespace ManageBidding.IoC
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ManageBiddingContext>();
+
+            #region Services
+            services.AddScoped<INotificationService, NotificationService>();
+            #endregion
+
+            #region Repositories
+            services.AddScoped<IBiddingRepository, BiddingRepository>();
+            #endregion
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
