6a778da99664d8ccbb7631a04c4e7494a790e036
Maurilio <maurilioz1@hotmail.com>
2023-11-25 15:11:02 -0300

    create bidding view model

---
+++ src/ManageBidding.Application/ViewModels/BiddingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ManageBidding.Core.DomainObjects.Enums;
+
+namespace ManageBidding.Application.ViewModels
+{
+    public class BiddingViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Number { get; set; }
+        public string? Description { get; set; }
+        public EStatus Status { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
