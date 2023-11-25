c35de655d5cf7ded941e2ad1f6c1a018e74edc3f
Maurilio <maurilioz1@hotmail.com>
2023-11-25 17:14:53 -0300

    create error action

--- src/ManageBidding.MVC/Controllers/HomeController.cs
+++ src/ManageBidding.MVC/Controllers/HomeController.cs
@@ -1,14 +1,14 @@
-using ManageBidding.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using ManageBidding.MVC.Models;
+using ManageBidding.Application.Interfaces.Services;
 
 namespace ManageBidding.MVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, INotificationService notificationService) : base(notificationService)
         {
             _logger = logger;
         }
@@ -23,10 +23,30 @@ namespace ManageBidding.MVC.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("erro/{id:length(3,3)}")]
+        public IActionResult Error(int id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var modelError = new ErrorViewModel();
+
+            if (id == 500)
+            {
+                modelError.ErrorCode = id;
+                modelError.Title = "Ocorreu um erro!";
+                modelError.Message = "Ocorreu um erro! Tente novamente mais tarde ou contate nosso suporte.";
+            }
+
+            if (id == 404)
+            {
+                modelError.ErrorCode = id;
+                modelError.Title = "Ops! Página não encontrada.";
+                modelError.Message = "A página que está procurando não existe! <br /> Em caso de dúvidas entre em contato com nosso suporte.";
+            }
+            else
+            {
+                return StatusCode(400);
+            }
+
+            return View("Error", modelError);
         }
     }
 }
