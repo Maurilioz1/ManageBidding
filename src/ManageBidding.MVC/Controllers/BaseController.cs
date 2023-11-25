e8885af723c1229601083db760f1a5ef05221004
Maurilio <maurilioz1@hotmail.com>
2023-11-25 16:55:39 -0300

    create base controller

---
+++ src/ManageBidding.MVC/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManageBidding.Application.Interfaces.Services;
+
+namespace ManageBidding.MVC.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        private readonly INotificationService _notificationService;
+
+        public BaseController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        protected bool ValidOperation()
+        {
+            return !_notificationService.HaveNotification();
+        }
+    }
+}
