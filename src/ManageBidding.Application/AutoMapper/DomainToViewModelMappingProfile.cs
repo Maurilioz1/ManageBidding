ae2826898dc902de2f00673b90484669cd3157b8
Maurilio <maurilioz1@hotmail.com>
2023-11-25 15:17:58 -0300

    create auto mapper map

---
+++ src/ManageBidding.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ManageBidding.Domain.Models;
+using ManageBidding.Application.ViewModels;
+
+namespace ManageBidding.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Bidding, BiddingViewModel>();
+        }
+    }
+}
---
+++ src/ManageBidding.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ManageBidding.Domain.Models;
+using ManageBidding.Application.ViewModels;
+
+namespace ManageBidding.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<BiddingViewModel, Bidding>().ConstructUsing(b => new Bidding(b.Number, b.Description, b.Status));
+        }
+    }
+}
