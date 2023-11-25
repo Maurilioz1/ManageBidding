493d983a485d0e027aaa7a3f41d69b48cb6901aa
Maurilio <maurilioz1@hotmail.com>
2023-11-25 15:23:39 -0300

    create interface bidding service

---
+++ src/ManageBidding.Application/Interfaces/Services/IBiddingService.cs
@@ -0,0 +1,14 @@
+﻿using ManageBidding.Domain.Models;
+using ManageBidding.Application.ViewModels;
+
+namespace ManageBidding.Application.Interfaces.Services
+{
+    public interface IBiddingService : IDisposable
+    {
+        Task<IEnumerable<BiddingViewModel>> Get();
+        Task<BiddingViewModel> GetById(Guid id);
+        Task<Bidding> Create(BiddingViewModel biddingViewModel);
+        Task<Bidding> Update(BiddingViewModel biddingViewModel);
+        Task<bool> Delete(Guid id);
+    }
+}
