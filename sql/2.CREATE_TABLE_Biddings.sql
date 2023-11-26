cf037ea2a536382f9ae9b118373f970493aaf1fc
Maurilio <maurilioz1@hotmail.com>
2023-11-26 20:10:23 -0300

    fix script css

--- sql/2.CREATE_TABLE_Biddings.sql
+++ sql/2.CREATE_TABLE_Biddings.sql
@@ -14,11 +14,14 @@ GO
 BEGIN TRANSACTION;
 GO
 
+CREATE SEQUENCE [BiddingSequence] AS int START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE NO CYCLE;
+GO
+
 CREATE TABLE [Biddings] (
     [Id] uniqueidentifier NOT NULL,
-    [Number] int NOT NULL IDENTITY,
+    [Number] int NOT NULL DEFAULT (NEXT VALUE FOR BiddingSequence),
     [Description] varchar(1000) NOT NULL,
-    [Status] char NOT NULL,
+    [Status] int NOT NULL,
     [RegistrationDate] datetime2 NOT NULL,
     [Active] bit NOT NULL,
     CONSTRAINT [PK_Biddings] PRIMARY KEY ([Id])
@@ -26,8 +29,9 @@ CREATE TABLE [Biddings] (
 GO
 
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20231126152229_Initial', N'8.0.0');
+VALUES (N'20231126204845_Initial', N'8.0.0');
 GO
 
 COMMIT;
 GO
+
