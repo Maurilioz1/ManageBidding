74c281b9b2c4c63cdc39c0784a8fb5ce90ce6aae
Maurilio <maurilioz1@hotmail.com>
2023-11-26 12:30:54 -0300

    fix connection string

---
+++ sql/CREATE_TABLE_BIDDINGS.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Biddings] (
+    [Id] uniqueidentifier NOT NULL,
+    [Number] int NOT NULL IDENTITY,
+    [Description] varchar(1000) NOT NULL,
+    [Status] char NOT NULL,
+    [RegistrationDate] datetime2 NOT NULL,
+    [Active] bit NOT NULL,
+    CONSTRAINT [PK_Biddings] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231126152229_Initial', N'8.0.0');
+GO
+
+COMMIT;
+GO
+
